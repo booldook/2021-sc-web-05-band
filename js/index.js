14da4468f4e454814a369109136d0f113d82d3b8
booldook <booldook@gmail.com>
2021-03-25 13:56:28 +0900

    ...

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ var swiper = new Swiper('.main-wrapper .swiper-container', {
 	loop: true,
 	effect: 'fade',
 	speed: 0,
-	autoplay: { delay: 5000 },
+	autoplay: { delay: 5000, disableOnInteraction: false },
 	navigation: {
 		nextEl: '.main-wrapper .bt-next',
 		prevEl: '.main-wrapper .bt-prev',
