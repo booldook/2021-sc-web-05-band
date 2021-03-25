6eb32ff8767bc1ce469cea37d433f33bd9183533
booldook <booldook@gmail.com>
2021-03-25 13:32:48 +0900

    main-wrapper

--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
 $('.bars-wrapper').click(function() {
 	$('.monavi-wrapper').toggleClass('active')
 })
+
+var swiper = new Swiper('.main-wrapper .swiper-container', {
+	loop: true,
+	effect: 'fade',
+	speed: 0,
+	autoplay: { delay: 5000 },
+	navigation: {
+		nextEl: '.main-wrapper .bt-next',
+		prevEl: '.main-wrapper .bt-prev',
+	},
+})
